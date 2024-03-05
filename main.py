605672d4ce5fc79c5e8d9824840b9b4d02d67814
jacobcd52 <jcdguitarguy@googlemail.com>
2024-03-05 15:06:40 -0500

    Add initial file to learning-vscode directory

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append("C:\\Users\\jcdgu\\vscode stuff\\learning-vscode")
+
+from src import addition
+from src import subtraction
+from src import multiplication
+from src import division
+
+print(addition.add(3,4))
+print(division.divide(3,4))
